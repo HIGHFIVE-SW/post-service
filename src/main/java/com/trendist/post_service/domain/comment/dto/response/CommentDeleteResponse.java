27455a50efa930058701a14485690549d39cdc5f
1winhyun <jimking1@naver.com>
2025-05-10 22:02:52 +0900

    refactor: 댓글 삭제, 수정 응답dto 수정

--- src/main/java/com/trendist/post_service/domain/comment/dto/response/CommentDeleteResponse.java
+++ src/main/java/com/trendist/post_service/domain/comment/dto/response/CommentDeleteResponse.java
@@ -8,15 +8,20 @@ import lombok.Builder;
 
 @Builder
 public record CommentDeleteResponse(
-	UUID postId,
 	UUID commentId,
+	UUID postId,
+	UUID reviewId,
 	UUID userId,
 	Boolean deleted
 ) {
 	public static CommentDeleteResponse from(Comment comment) {
+		UUID postId = comment.getPost() != null ? comment.getPost().getId() : null;
+		UUID reviewId = comment.getReview() != null ? comment.getReview().getId() : null;
+
 		return CommentDeleteResponse.builder()
-			.postId(comment.getPost().getId())
 			.commentId(comment.getId())
+			.postId(postId)
+			.reviewId(reviewId)
 			.userId(comment.getUserId())
 			.deleted(comment.getDeleted())
 			.build();
--- src/main/java/com/trendist/post_service/domain/comment/dto/response/CommentUpdateResponse.java
+++ src/main/java/com/trendist/post_service/domain/comment/dto/response/CommentUpdateResponse.java
@@ -11,14 +11,19 @@ import lombok.Builder;
 public record CommentUpdateResponse(
 	UUID commentId,
 	UUID postId,
+	UUID reviewId,
 	UUID userId,
 	String content,
 	LocalDateTime updatedAt
 ) {
 	public static CommentUpdateResponse from(Comment comment) {
+		UUID postId = comment.getPost() != null ? comment.getPost().getId() : null;
+		UUID reviewId = comment.getReview() != null ? comment.getReview().getId() : null;
+
 		return CommentUpdateResponse.builder()
 			.commentId(comment.getId())
-			.postId(comment.getPost().getId())
+			.postId(postId)
+			.reviewId(reviewId)
 			.userId(comment.getUserId())
 			.content(comment.getContent())
 			.updatedAt(comment.getUpdatedAt())
